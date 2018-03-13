d5ad09fee052c16014c1ac5f2258029ee3cd15fa
Полина <@LENOVO-PC>
2018-03-13 21:47:32 +0300

    Добавьте файлы проекта.

---
+++ Веб-камера/Веб-камера.Windows/CommandPhoto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.Media.Capture;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Веб_камера
+{
+    public class CommandPhoto : ICommand
+    {
+
+
+        public async void Execute(object parameter)
+        {
+            var vm = parameter as ViewModel;
+            var ui = new CameraCaptureUI();
+            //Пропорции фотографии
+            ui.PhotoSettings.CroppedAspectRatio = new Windows.Foundation.Size(16, 9);
+
+            var file = await ui.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            if (file != null)
+            {
+                var bitmap = new BitmapImage();
+                var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                bitmap.SetSource(stream);
+                vm.ImageSource = bitmap;
+
+            }
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+    }
+   }
+
---
+++ Веб-камера/Веб-камера.Windows/ViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Веб_камера
+{
+  public class ViewModel: INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+            public void DoPropertyChanged(String name)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(name));
+                }
+            }
+            public ICommand DoPhoto { get; set; }
+            private object imageSource { get; set; }
+
+            public object ImageSource
+        {
+            get { return imageSource; }
+            set
+            {
+                imageSource = value;
+                DoPropertyChanged("ImageSource");
+            }
+               
+        }
+
+
+        public ViewModel()
+        {
+            DoPhoto = new CommandPhoto();
+        }
+    }
+    
+    }
+
